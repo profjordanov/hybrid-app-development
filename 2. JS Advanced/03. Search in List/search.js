1213d42619cd0a0a66dfc167664cce0106811a0c
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-01 01:53:57 +0200

    init

--- 2. JS Advanced/03. Search in List/search.js
+++ 2. JS Advanced/03. Search in List/search.js
@@ -1,15 +1,3 @@
 function search() {
-    let text = $('#searchText').val();
-    let matches = 0;
 
-    for(let town of $('#towns li').toArray()){
-        if(town.textContent.indexOf(text) != -1){
-            matches++;
-            $(town).css('font-weight', 'bold');
-        } else {
-            $(town).css('font-weight', '');
-        }
-    }
-
-    $('#result').text(matches + ' matches found.')
 }
