474e032e6789692327dcae53c4b3d21ead763978
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-04 09:53:23 +0200

    init

--- 05. KinveyStudents/KinveyStudents/www/scripts/index.js
+++ 05. KinveyStudents/KinveyStudents/www/scripts/index.js
@@ -1,8 +1,4 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+﻿(function () {
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
@@ -12,19 +8,13 @@
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        loadStudents();
     };
 
     function onPause() {
-        // TODO: This application has been suspended. Save application state here.
     };
 
     function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
     };
+
 } )();
---
+++ 05. KinveyStudents/KinveyStudents/www/scripts/students.js
@@ -0,0 +1,36 @@
+﻿const baseSurviceUrl = "https://baas.kinvey.com/appdata/kid_BJXTsSi-e/students";
+const kinveyUsername = "guest";
+const kinveyPassword = "guest";
+const base64Auth = btoa(kinveyUsername + ":" + kinveyPassword);
+
+function loadStudents() {
+    $.ajax({
+        type: "GET",
+        url: baseSurviceUrl,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + base64Auth);
+        },
+        success: function (students) {
+            displayStudents(students);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus);
+            alert("Error: " + errorThrown);
+        }
+    });
+}
+
+function displayStudents(students) {
+    $('#results').find('tr').nextAll().remove();
+
+    students = students.sort((a, b) => a.ID - b.ID);
+    //students = students.slice(0, 10);
+
+    for (let student of students) {
+        $("#results")
+            .append($('<tr>')
+                .append($('<td>').text(student.FirstName))
+                .append($('<td>').text(student.Grade))
+            );
+    }
+}
