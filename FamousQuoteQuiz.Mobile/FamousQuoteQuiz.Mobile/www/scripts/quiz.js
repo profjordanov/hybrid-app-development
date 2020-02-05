2db9ccdf4dfd6248301c8c125b24907b1943ec0b
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-05 17:00:13 +0200

    init

---
+++ FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/quiz.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    const querydata = {
+        initialId: 0
+    };
+
+    const url = baseSurviceQuizUrl + "binary-choice-question" + encodeQueryData(querydata);
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        responseType: 'application/json',
+        success: function (data) {
+            applyBinaryChoiceQuestion(data);
+        },
+        error: function () {
+            alert("Err");
+        }
+    });
+
+})(baseSurviceQuizUrl);
+
+function applyBinaryChoiceQuestion(data) {
+    $("#bin-quote-id").text(data.id);
+    $("#bin-quote-isTrue").text(data.isTrue);
+    $("#bin-quote-content").text(data.quote);
+    $("#bin-quote-author").text(data.questionableAuthor);
+    $("#bin-quote-correct-author").text(data.correctAuthor);
+}
