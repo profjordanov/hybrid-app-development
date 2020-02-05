2cf861a7122bcd64dbd30242e693e302d50d0b9d
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-05 15:59:20 +0200

    init

---
+++ FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/encodeQueryData.js
@@ -0,0 +1,7 @@
+﻿function encodeQueryData(data) {
+    let ret = [];
+    for (let d in data)
+        ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+    ret = ret.join("&");
+    return "?" + ret;
+}
---
+++ FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/getLastQuestions.js
@@ -0,0 +1,37 @@
+﻿const baseSurviceQuizUrl = "http://localhost:5000/api/quiz/";
+
+(function () {
+    $.ajax({
+        url: baseSurviceQuizUrl + "last-binary-choice-question",
+        type: 'GET',
+        responseType: 'application/json',
+        success: function (data) {
+            storeLastBinaryChoiceQuestionId(data);
+        },
+        error: function () {
+            alert("ERROR!");
+        }
+    });
+})(baseSurviceQuizUrl);
+
+(function () {
+    $.ajax({
+        url: baseSurviceQuizUrl + "last-multiple-choice-question",
+        type: 'GET',
+        responseType: 'application/json',
+        success: function (data) {
+            storeLastMultipleChoiceQuestionId(data);
+        },
+        error: function () {
+            alert("ERROR!");
+        }
+    });
+})(baseSurviceQuizUrl);
+
+function storeLastBinaryChoiceQuestionId(response) {
+    localStorage.setItem('lastBinaryChoiceQuestionId', response.id);
+}
+
+function storeLastMultipleChoiceQuestionId(response) {
+    localStorage.setItem('lastMultipleChoiceQuestionId', response.id);
+}
