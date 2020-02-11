83a04e2d30bcc19aca0ceb8274736bd00f69bbc3
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-11 09:29:38 +0200

    Update quiz.js

--- FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/quiz.js
+++ FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/quiz.js
@@ -1,4 +1,6 @@
-﻿(function () {
+﻿// BEGIN Binary Choice Question Logic Section
+
+(function () {
     const querydata = {
         initialId: 0
     };
@@ -26,3 +28,53 @@ function applyBinaryChoiceQuestion(data) {
     $("#bin-quote-author").text(data.questionableAuthor);
     $("#bin-quote-correct-author").text(data.correctAuthor);
 }
+
+// END Binary Choice Question Logic Section
+
+// BEGIN Multiple Choice Question Logic Section
+
+(function () {
+    const querydata = {
+        initialId: 0
+    };
+
+    const url = baseSurviceQuizUrl + "multiple-choice-question" + encodeQueryData(querydata);
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        responseType: 'application/json',
+        success: function (data) {
+            applyMultipleChoiceQuestion(data);
+        },
+        error: function () {
+            $.fancybox("#error");
+        }
+    });
+
+})(baseSurviceQuizUrl);
+
+function applyMultipleChoiceQuestion(data) {
+    $("#mult-quest-id").text(data.id);
+    $("#correct-author-id").text(data.correctAuthorId);
+    $("#mult-quote-content").text(data.quote);
+    let authors = data.authors;
+
+    var correctAuthor = authors.find(function (element) {
+        return element.id == data.correctAuthorId;
+    });
+
+    $("#correct-author-name").text(correctAuthor.name);
+
+    $("#authorX").text(authors[0].name);
+    $("#authorX").attr('authorId', authors[0].id);
+
+    $("#authorY").text(authors[1].name);
+    $("#authorY").attr('authorId', authors[1].id);
+
+    $("#authorZ").text(authors[2].name);
+    $("#authorZ").attr('authorId', authors[2].id);
+}
+
+
+// END Multiple Choice Question Logic Section
