ba336eff35fcb51d1abac5727313dfc479039c4b
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-11 11:18:12 +0200

    finished

--- FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/quiz.js
+++ FamousQuoteQuiz.Mobile/FamousQuoteQuiz.Mobile/www/scripts/quiz.js
@@ -39,14 +39,14 @@ $("#true-bin-answ-btn").click(function () {
 
     if (questIsTrue == "true") {
         if (currentQuestionId == lastBinaryChoiceQuestionId) {
-            showFinalCorrectUserAnswer(correctAuthor);
+            showBcqFinalCorrectUserAnswer(correctAuthor);
         } else {
-            showCorrectUserAnswer(correctAuthor);
+            showBcqCorrectUserAnswer(correctAuthor);
             showNextBcqBtn();
         }
     } else {
         if (currentQuestionId == lastBinaryChoiceQuestionId) {
-            showFinalIncorrectUserAnswer(correctAuthor);
+            showBcqFinalIncorrectUserAnswer(correctAuthor);
         } else {
             showIncorrectUserAnswer(correctAuthor);
             showNextBcqBtn();
@@ -61,16 +61,16 @@ $("#false-bin-answ-btn").click(function () {
     const lastBinaryChoiceQuestionId = localStorage.lastBinaryChoiceQuestionId;
     if (questIsTrue == "true") {
         if (currentQuestionId == lastBinaryChoiceQuestionId) {
-            showFinalIncorrectUserAnswer(correctAuthor);
+            showBcqFinalIncorrectUserAnswer(correctAuthor);
         } else {
-            showIncorrectUserAnswer(correctAuthor);
+            showBcqIncorrectUserAnswer(correctAuthor);
             showNextBcqBtn();
         }
     } else {
         if (currentQuestionId == lastBinaryChoiceQuestionId) {
-            showFinalCorrectUserAnswer(correctAuthor);
+            showBcqFinalCorrectUserAnswer(correctAuthor);
         } else {
-            showCorrectUserAnswer(correctAuthor);
+            showBcqCorrectUserAnswer(correctAuthor);
             showNextBcqBtn();
         }
     }
@@ -119,22 +119,22 @@ function showYesNoBtns() {
 
 // Popups logic
 
-function showCorrectUserAnswer(correctAuthor) {
+function showBcqCorrectUserAnswer(correctAuthor) {
     $("#bcq-correct-author-answer").text(correctAuthor);
     $("#bcq-correct-user-answer-popup").popup("open");
 }
 
-function showIncorrectUserAnswer(correctAuthor) {
+function showBcqIncorrectUserAnswer(correctAuthor) {
     $("#bcq-incorrect-author-answer").text(correctAuthor);
     $("#bcq-incorrect-user-answer-popup").popup("open");
 }
 
-function showFinalCorrectUserAnswer(correctAuthor) {
+function showBcqFinalCorrectUserAnswer(correctAuthor) {
     $("#bcq-correct-author-final-answer").text(correctAuthor);
     $("#bcq-correct-user-answer-quiz-end-popup").popup("open");
 }
 
-function showFinalIncorrectUserAnswer(correctAuthor) {
+function showBcqFinalIncorrectUserAnswer(correctAuthor) {
     $("#bcq-incorrect-author-final-answer").text(correctAuthor);
     $("#bcq-incorrect-user-answer-quiz-end-popup").popup("open");
 }
@@ -186,5 +186,104 @@ function applyMultipleChoiceQuestion(data) {
     $("#authorZ").attr('authorId', authors[2].id);
 }
 
+// Buttons logic
+$("#authorX").click(function () {
+    const authorXid = $("#authorX").attr('authorId');
+    checkCorrectAuthorById(authorXid);
+});
+
+$("#authorY").click(function () {
+    const authorYid = $("#authorY").attr('authorId');
+    checkCorrectAuthorById(authorYid);
+});
+
+$("#authorZ").click(function () {
+    const authorZid = $("#authorZ").attr('authorId');
+    checkCorrectAuthorById(authorZid);
+});
+
+$("#next-mult-quest-btn").click(function () {
+    const currentQuoteId = $("#mult-quest-id").text();
+
+    const querydata = {
+        initialId: currentQuoteId
+    };
+
+    const url = baseSurviceQuizUrl + "multiple-choice-question" + encodeQueryData(querydata);
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        responseType: 'application/json',
+        success: function (data) {
+            applyMultipleChoiceQuestion(data);
+            showAnswersBtns();
+        },
+        error: function () {
+            $.fancybox("#error");
+        }
+    });
+});
+
+function checkCorrectAuthorById(authorNid) {
+    const questId = $("#mult-quest-id").text();
+    const correctAuthorId = $("#correct-author-id").text();
+    const correctAuthorName = $("#correct-author-name").text();
+    const lastMultipleChoiceQuestionId = localStorage.lastMultipleChoiceQuestionId;
+
+    if (correctAuthorId == authorNid) {
+        if (questId == lastMultipleChoiceQuestionId) {
+            showMcqFinalCorrectUserAnswer(correctAuthorName);
+        } else {
+            showMcqCorrectUserAnswer(correctAuthorName);
+            showNextMultQuestBtn();
+        }
+    } else {
+        if (questId == lastMultipleChoiceQuestionId) {
+            showMcqFinalIncorrectUserAnswer(correctAuthorName);
+        } else {
+            showMcqIncorrectUserAnswer(correctAuthorName);
+            showNextMultQuestBtn();
+        }
+    }
+}
+
+function showNextMultQuestBtn() {
+    const correctAuthor = $("#correct-author-name").text();
+    $("#mult-quote-correct-answer").show();
+    $("#mult-quote-correct-answer").text("by " + correctAuthor);
+
+    $("#next-mult-quest-btn").show();
+    $("#answers-buttons").hide();
+}
+
+function showAnswersBtns() {
+    $("#mult-quote-correct-answer").hide();
+
+    $("#answers-buttons").show();
+    $("#next-mult-quest-btn").hide();
+}
+
+// Popups logic
+
+function showMcqCorrectUserAnswer(correctAuthor) {
+    $("#mcq-correct-author-answer").text(correctAuthor);
+    $("#mcq-correct-user-answer-popup").popup("open");
+}
+
+function showMcqIncorrectUserAnswer(correctAuthor) {
+    $("#mcq-incorrect-author-answer").text(correctAuthor);
+    $("#mcq-incorrect-user-answer-popup").popup("open");
+}
+
+function showMcqFinalCorrectUserAnswer(correctAuthor) {
+    $("#mcq-correct-author-final-answer").text(correctAuthor);
+    $("#mcq-correct-user-answer-quiz-end-popup").popup("open");
+}
+
+function showMcqFinalIncorrectUserAnswer(correctAuthor) {
+    $("#mcq-incorrect-author-final-answer").text(correctAuthor);
+    $("#mcq-incorrect-user-answer-quiz-end-popup").popup("open");
+}
 
 // END Multiple Choice Question Logic Section
