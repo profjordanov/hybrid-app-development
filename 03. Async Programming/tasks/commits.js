e4f6a96db8d2a0039fecde6a437007e3d4c12555
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-14 14:39:28 +0200

    init

---
+++ 03. Async Programming/tasks/commits.js
@@ -0,0 +1,18 @@
+function loadCommits() {
+    // ensure unordered list is empty
+
+    // get username value
+    // get repo value
+
+    // create the request url
+
+    // execute the AJAX
+}
+
+function displayCommits(commits) {
+
+}
+
+function displayError(err) {
+
+}
