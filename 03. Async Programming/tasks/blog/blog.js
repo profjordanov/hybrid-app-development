f54639f6c9681bcc889b0b37e8757e8468536b3e
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-14 15:15:40 +0200

    v rusia sum

--- 03. Async Programming/tasks/blog/blog.js
+++ 03. Async Programming/tasks/blog/blog.js
@@ -5,24 +5,38 @@ function attachEvents() {
     const kinveyPassword = "p";
     const base64auth = btoa(kinveyUsername + ":" + kinveyPassword);
     const authHeaders = { "Authorization": "Basic " + base64auth };
+
     $('#btnLoadPosts').click(loadPostsClick);
     $('#btnViewPost').click(viewPostClick);
 
     function loadPostsClick() {
+
         let loadPostsRequest = {
             url: serviceUrl + "/posts",
             headers: authHeaders
         };
 
-        $.ajax(loadPostsRequest)
-            .then(displayPosts)
-            .catch(displayError);
+        // execute AJAX
 
         function displayPosts(posts) {
-            $('#posts').empty();
+            // ensure the dropdown is empty
+            // for each post append optins in the dropdown menu
+        }
+    }
 
-            for (let post of posts) {
-                $('#posts').append($('<option>').text(post.title).val(post._id));
+    function viewPostClick() {
+        // get the selected post id
+
+        // require post details and its comments
+
+        function displayPostWithComments([post, comments]) {
+            $('#post-title').text(post.title);
+            $('#post-body').text(post.body);
+            $('#post-comments').empty();
+
+            for (let comment of comments) {
+
+                $('#post-comments').append($('<li>').text(comment.text));
             }
         }
     }
@@ -36,37 +50,5 @@ function attachEvents() {
                 $(errorDiv).remove();
             });
         }, 3000);
-    }
-
-    function viewPostClick() {
-        let selectedPostId = $('#posts').val();
-        if (!selectedPostId) {
-            return;
-        }
-
-        let requestPosts = $.ajax({
-            url: serviceUrl + "/posts/" + selectedPostId,
-            headers: authHeaders
-        });
-
-        let requestComments = $.ajax({
-            url: serviceUrl + `/comments/?query={"post_id":"${selectedPostId}"}`,
-            headers: authHeaders
-        });
-
-        Promise.all([requestPosts, requestComments])
-            .then(displayPostWithComments)
-            .catch(displayError);
-
-        function displayPostWithComments([post, comments]) {
-            $('#post-title').text(post.title);
-            $('#post-body').text(post.body);
-            $('#post-comments').empty();
-
-            for (let comment of comments) {
-
-                $('#post-comments').append($('<li>').text(comment.text));
-            }
-        }
     }
 }
