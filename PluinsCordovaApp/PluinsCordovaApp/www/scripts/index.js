613ed9a3817f94d945812b82813de13ae5f39a62
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-24 10:22:32 +0200

    init

--- PluinsCordovaApp/PluinsCordovaApp/www/scripts/index.js
+++ PluinsCordovaApp/PluinsCordovaApp/www/scripts/index.js
@@ -15,6 +15,26 @@
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
+        // network-information
+
+        function checkConnection() {
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN] = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI] = 'WiFi connection';
+            states[Connection.CELL_2G] = 'Cell 2G connection';
+            states[Connection.CELL_3G] = 'Cell 3G connection';
+            states[Connection.CELL_4G] = 'Cell 4G connection';
+            states[Connection.CELL] = 'Cell generic connection';
+            states[Connection.NONE] = 'No network connection';
+
+            alert('Connection type: ' + states[networkState]);
+        }
+
+        checkConnection();
+
         // geolocation
         // onSuccess Callback
         //   This method accepts a `Position` object, which contains
