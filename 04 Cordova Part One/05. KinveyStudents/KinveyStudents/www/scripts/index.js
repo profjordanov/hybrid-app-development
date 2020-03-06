b7663e53363efc1527eb6ebbb6df8ea7a55ca439
Jordan Jordanov <d_yugioh@abv.bg>
2020-03-06 09:03:46 +0200

    init

---
+++ 04 Cordova Part One/05. KinveyStudents/KinveyStudents/www/scripts/index.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        loadStudents();
+    };
+
+    function onPause() {
+    };
+
+    function onResume() {
+    };
+
+} )();
