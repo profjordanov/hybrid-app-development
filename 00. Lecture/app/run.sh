d93ea695224b36499685f1f8d8a9c114d88b5e76
Jordan Jordanov <d_yugioh@abv.bg>
2023-03-20 11:06:17 +0200

    Update run.sh

--- 00. Lecture/app/run.sh
+++ 00. Lecture/app/run.sh
@@ -2,6 +2,6 @@ docker build --tag profjordanov/smplapp:1.0 .
 
 docker run --publish 8080:8080 --detach --name smplapp profjordanov/smplapp:1.0
 
-docker run --publish 8080:8080 profjordanov/smplapp:1.0
+docker run -p 8080:8080 profjordanov/smplapp:1.0
 
 docker push profjordanov/smplapp:1.0
