9f1bafd77caad1325f9dbc83b91b138ed06e6f30
Jordan Jordanov <d_yugioh@abv.bg>
2025-02-25 16:13:01 +0200

    Create index.js

---
+++ 02. Алгоритми и Структури/index.js
@@ -0,0 +1,57 @@
+function printHelloWorld() {
+  return 'Hello World!';
+}
+-------------------------
+function getSum(a, b) {
+  return a + b;
+}
+--------------------------
+function calculator(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      throw new Error('Invalid operator');
+  }
+
+  return result;
+}
+---------------------------------
+
+  function countOccurrences(str, char) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+-------------------------------------
+
+function findMaxNumber(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
