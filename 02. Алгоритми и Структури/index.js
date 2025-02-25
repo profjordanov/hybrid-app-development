c1896c78230c8863f5048b9340489d7921a548a4
Jordan Jordanov <d_yugioh@abv.bg>
2025-02-25 16:18:56 +0200

    Update index.js

--- 02. Алгоритми и Структури/index.js
+++ 02. Алгоритми и Структури/index.js
@@ -55,3 +55,93 @@ function findMaxNumber(arr) {
 
   return max;
 }
+-----------------------------------
+function titleCase(str) {
+  const words = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(' ');
+}
+---------------------------------------
+function reverseString(str) {
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
+---------------------------------------
+ function isPalindrome(str) {
+  const formattedStr = removeNonAlphanumeric(str.toLowerCase());
+  const reversedStr = reverseString(formattedStr);
+  return formattedStr === reversedStr;
+}
+
+function removeNonAlphanumeric(str) {
+  let formattedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (isAlphaNumeric(char)) {
+      formattedStr += char;
+    }
+  }
+  return formattedStr;
+}
+
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) || // Numbers 0-9
+    (code >= 97 && code <= 122) // Lowercase letters a-z
+  );
+}
+
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+_____________________________________
+function countVowels(str) {
+  const formattedStr = str.toLowerCase();
+  let count = 0;
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr[i];
+
+    if (
+      char === 'a' ||
+      char === 'e' ||
+      char === 'i' ||
+      char === 'o' ||
+      char === 'u'
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
+-------------------------------------------
+ function removeDuplicates(arr) {
+  const uniqueArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+
+  return uniqueArr;
+} 
+
+  
+  
